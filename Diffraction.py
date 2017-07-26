66fbe4721c2e4d080a2e6022de51f595f64ea602
JPBGoe <JPBGoe@users.noreply.github.com>
2017-07-26 08:11:04 +0200

    not working

--- Diffraction.py
+++ Diffraction.py
@@ -3,53 +3,82 @@ from copy import deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
 
-class DiffException(Exception):
-    pass
+# the cartesian cross is centered in the input image
 
 
 class Diffraction:
-    __dist = 0
-    __amp_0 = []
-    __phase_0 = []
-    __res = []
-    __wavevec = 0
+    dist = 0
+    start_amp = []
+    start_phase = []
+    start_pos = []
+    fin_amp = []
+    fin_phase = []
+    fin_pos = []
+    wavevec = 1 
+
+
+    res = []
+
+    def set_input(self,ini_amp,ini_pos):
+        self.start_amp = np.abs(ini_amp)
+        self.start_phase = np.angle(ini_amp)
+        self.start_pos = ini_pos
+
+    def set_output(self,fin_pos):
+        self.fin_pos = fin_pos
+
+    def set_dist(self, dist):
+        self.dist = dist
+
+    def sphere_wave(self,origin,at):    #origin =(x_ind,z_ind) with respect to the image origin not the center
+        # calculate the distance
+        distance = math.sqrt((fin_pos[at[0]]-start_pos[origin[0]])**2 + dist**2 + (fin_pos[at[1]]-start_pos[origin[1]])**2)
+        
+        # calculate the object origin
+        obj_ori = (len(start_amp) // 2,len(start_amp[0]) // 2)
+
+        # phase
+        phase = ( (wavevec * distance) % (math.pi*2) ) + startphase[origin(0) + obj_ori(0)][origin(1)+obj_ori(1)]
+        phase = ( phase % (math.pi*2))
+
+        # final amplitude at the place "at"
+        return (math.cos(phase) + 1j*math.sin(phase)) * start_amp[origin(0) + obj_ori(0)][origin(1)+obj_ori(1)]
+
+
+        
+
 
     
-    def __init__(self,dist):
-        self.__dist = dist              # image and object plane distance
-
-    
-    def __wave(self,ori,dest):          # relative to the major optical axis in the center of __amp_0
-        ori = ori                       # x component, z component
-        dest = dest                     # x component, z component
+    def wave(self,ori,dest):          # relative to the major optical axis in the center of __amp_0
+        if self.amp_0[ori[0]][ori[1]] == 0.0:
+            return 0
 
         # calculate the distances
-        r = [(dest[0]-ori[0]),self.__dist,(dest[1]-ori[1])]
+        r = [(dest[0]-ori[0]),self.dist,(dest[1]-ori[1])]
         R = math.sqrt(r[0]**2 + r[1]**2 + r[2]**2)
-        #print(str(r[0]) + " " + str(r[1]) + " "  + str(r[2]) + " " + str(R))
 
         # correct for relative position
-        ori[0] = ori[0] + len(self.__amp_0) // 2        # x coordinate
-        ori[1] = ori[1] + len(self.__amp_0[0]) // 2     # z coordinate
-        dest[0] = dest[0] + len(self.__res) // 2        # x coordinate
-        dest[1] = dest[1] + len(self.__res[0]) // 2     # z coordinate
-        if not (0 <= ori[0] and ori[0] < len(self.__amp_0)):
+        ori[0] = ori[0] + len(self.amp_0) // 2        # x coordinate
+        ori[1] = ori[1] + len(self.amp_0[0]) // 2     # z coordinate
+        dest[0] = dest[0] + len(self.res) // 2        # x coordinate
+        dest[1] = dest[1] + len(self.res[0]) // 2     # z coordinate
+        if not (0 <= ori[0] and ori[0] < len(self.amp_0)):
             print("ori[0]=" + str(ori[0]))
-        if not (0 <= ori[1] and ori[1] < len(self.__amp_0[0])):
+        if not (0 <= ori[1] and ori[1] < len(self.amp_0[0])):
             print("ori[0]=" + str(ori[1]))
 
         # calculate the phase for a homogenious propagation
-        phase = self.__wavevec * R + self.__phase_0[ori[0]][ori[1]] # - w*t
+        phase = self.wavevec * R + self.phase_0[ori[0]][ori[1]] # - w*t
         phase = (phase % (2*math.pi))
 
         # Calculate the complex amplitude at the destination
-        return (math.cos(phase) + 1j* math.sin(phase))*self.__amp_0[ori[0]][ori[1]]
+        return (math.cos(phase) + 1j* math.sin(phase))*self.amp_0[ori[0]][ori[1]]/R
     
 
     def calculate(self,amp_0,phase_0,img_size,wavevec):
-        self.__amp_0 = amp_0
-        self.__phase_0 = phase_0
-        self.__wavevec = wavevec
+        self.amp_0 = amp_0
+        self.phase_0 = phase_0
+        self.wavevec = wavevec
 
         # init of the image plain
         tmp = []
@@ -57,31 +86,43 @@ class Diffraction:
             tmp.append(0 + 1j*0)
 
         for j in range(img_size[1]):
-            self.__res.append(deepcopy(tmp))
+            self.res.append(deepcopy(tmp))
 
         # calculate
-        for i in list(range(len(self.__res))):
-            for j in list(range(len(self.__res[0]))):
-                for k in list(range(len(self.__amp_0))):
-                    for l in list(range(len(self.__amp_0[0]))):
-                        rel_i = i - len(self.__res) // 2
-                        rel_j = j - len(self.__res[0]) // 2
-                        rel_k = k - len(self.__amp_0) // 2
-                        rel_l = l - len(self.__amp_0[0]) // 2
-                        self.__res[i][j] += self.__wave([rel_k,rel_l],[rel_i,rel_j])
-      
+        for i in list(range(len(self.res))):
+            for j in list(range(len(self.res[0]))):
+                for k in list(range(len(self.amp_0))):
+                    for l in list(range(len(self.amp_0[0]))):
+                        rel_i = i - len(self.res) // 2
+                        rel_j = j - len(self.res[0]) // 2
+                        rel_k = k - len(self.amp_0) // 2
+                        rel_l = l - len(self.amp_0[0]) // 2
+                        self.res[i][j] += self.wave([rel_k,rel_l],[rel_i,rel_j])
+        return self.res        
+        
+        
+    
 
-        return self.__res        
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+    
 # Testing
 
-osi = [21,21]
-fsi = [40,40]
+osi = [30,30]
+fsi = [100,100]
 tmp = []
 for i in list(range(osi[1])):
-    tmp.append(1.0)
+    tmp.append(0.0)
 
 ph = []
 amp = []
@@ -89,17 +130,31 @@ for j in list(range(osi[0])):
     ph.append(deepcopy(tmp))
     amp.append(deepcopy(tmp))
 
+for i in list(range(osi[0])):
+    for j in list(range(osi[1])):
+        if (i-osi[0]//2)**2 + (j-osi[1]//2)**2 <= 5**2:
+            amp[i][j] = 1.0
 
-ft = np.fft.fft2(amp)
-plt.figure(0)
-plt.imshow(np.abs(ft))
-plt.colorbar()
-
-test = Diffraction(10000)
-result = test.calculate(amp,ph,fsi,2*math.pi*10**9/600)
+#print(amp)
 
 
 plt.figure(1)
+plt.imshow(amp)
+plt.colorbar()
+
+
+ft = np.fft.fft2(amp)
+#print(len(ft))
+plt.figure(2)
+plt.imshow(np.abs(ft))
+plt.colorbar()
+
+test = Diffraction()
+test.set_dist(1000)
+result = test.calculate(amp,ph,fsi,2*math.pi*10**9/600)
+
+
+plt.figure(3)
 plt.imshow(np.abs(result))
 plt.colorbar()
 plt.show()
